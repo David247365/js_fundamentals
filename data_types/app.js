1373a52552665523d5057394c198b1dbfbd93026
David Soto <ds051128@gmail.com>
2022-06-15 02:56:10 -0500

    Data Types - Numbers

--- data_types/app.js
+++ data_types/app.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+// Data Types
+
+// Numbers & Arithmetic Operations
+console.log(1 + 2);
+console.log(2 - 1);
+console.log(2 * 2);
+console.log(9 / 3);
+console.log(10 % 5);
