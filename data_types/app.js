2084b7e506aa1debc3f354e0f9ad518a3bb6889d
David Soto <ds051128@gmail.com>
2022-06-15 03:18:47 -0500

    variables

--- data_types/app.js
+++ data_types/app.js
@@ -11,3 +11,17 @@ console.log(2 * 2);
 console.log(9 / 3);
 console.log(10 % 5);
 
+// Boolean (True False) & Boolean Operations (&& || !) 
+
+console.log(!false);
+console.log(false && false);
+console.log(true || false);
+
+//  Variables - data containers
+
+let firstName = "David";
+let lastName = "Soto";
+let age;
+console.log(firstName, lastName, age);
+age = 26;
+console.log(firstName, lastName, age);
