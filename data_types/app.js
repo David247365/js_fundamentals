f1da328aea9b23cb75ad210d217f34afa9e61996
David Soto <ds051128@gmail.com>
2022-06-15 03:11:15 -0500

    booleans

--- data_types/app.js
+++ data_types/app.js
@@ -1,6 +1,8 @@
-console.log("Hello World");
-
 // Data Types
+
+// Strings
+
+console.log("Hello World");
 
 // Numbers & Arithmetic Operations
 console.log(1 + 2);
@@ -8,3 +10,4 @@ console.log(2 - 1);
 console.log(2 * 2);
 console.log(9 / 3);
 console.log(10 % 5);
+
